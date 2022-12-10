41b1e8a6c6e2a27495df34b4fd64c634c7c441c9
Amanda Michelle Hecht <amandahecht89@gmail.com>
2022-12-10 11:19:38 -0700

    deck of card renders and shuffles in console logs

--- app.js
+++ app.js
@@ -11,8 +11,28 @@ let canHit = true;
 
 window.onload = function() {
     buildDeck();
+    shuffleDeck();
 };
 
 function buildDeck() {
+    let values = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'];
+    let types = ['C', 'H', 'S', 'D'];
+    deck = [];
 
+    for (let i = 0; i < types.length; i++) {
+        for (let j = 0; j < values.length; j++) {
+            deck.push(values[j] + '-' + types[i]);
+        }
+    }
 }
+
+function shuffleDeck() {
+    for (let i = 0; i < deck.length; i++) {
+        let j = Math.floor(Math.random() * deck.length);
+        let temp = deck[i];
+        deck[i] = deck[j];
+        deck[j] = temp;
+    }
+}
+
+
