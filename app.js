84fce44bd4d5214601b65fab8c79dc703e3a4041
Amanda Michelle Hecht <amandahecht89@gmail.com>
2022-12-10 11:06:08 -0700

    remane playing cards and some javascript

---
+++ app.js
@@ -0,0 +1,18 @@
+let dealerSum = 0;
+let yourSum = 0;
+
+let dealerAceCount = 0;
+let yourAceCount = 0;
+
+let hidden;
+let deck;
+
+let canHit = true;
+
+window.onload = function() {
+    buildDeck();
+};
+
+function buildDeck() {
+
+}
